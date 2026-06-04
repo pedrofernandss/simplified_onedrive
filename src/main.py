1ee174095d1daaf3301f066c52e512d3d428c9e9
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-06-04 17:07:18 -0300

    feat: execution test

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 import os
 import time
+from network.discovery import DiscoveryService
 
 node_id = os.environ.get("NODE_ID", "No_Desconhecido")
-sync_dir = os.environ.get("SYNC_DIR", "/app/sync_folder")
+sync_dir = os.environ.get("SYNC_DIR", "/app/documents")
 
-print(f"[{node_id}] Contêiner iniciado com sucesso!")
+print(f"[{node_id}] Conteiner iniciado com sucesso!")
 print(f"[{node_id}] Monitorando a pasta virtual: {sync_dir}")
-print(f"[{node_id}] Aguardando arquivos... (Pressione Ctrl+C para sair)\n")
 
-while True:
-    time.sleep(10)
+discovery = DiscoveryService(node_id)
+discovery.start()
+
+print(f"[{node_id}] Sistema ativo. Pressione Ctrl+C para sair.\n")
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    print(f"\n[{node_id}] A desligar os motores...")
+    discovery.running = False
