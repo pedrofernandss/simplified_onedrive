7e6daa4366f5de19144d71975d9e929f524aed65
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-06-05 18:09:41 -0300

    refactor: typing code

--- src/core/node.py
+++ src/core/node.py
@@ -2,20 +2,20 @@ from core.file_monitor import FileMonitor
 from network.discovery import DiscoveryService
 
 class Node:
-    def __init__(self, node_id, sync_dir):
+    def __init__(self, sync_dir: str, node_id: str):
         self.node_id = node_id
         self.sync_dir = sync_dir
 
         self.discovery = DiscoveryService(self.node_id)
         self.monitor = FileMonitor(self.sync_dir, self.node_id)
 
-    def start(self):
+    def start(self) -> None:
         self.discovery.start()
         self.monitor.start()
 
-        print(f"[{self.node_id}] Nó operacional. Aguardando eventos...\n")
+        print(f"[{self.node_id}] em operação. Aguardando eventos...\n")
 
-    def stop(self):
-        print(f"\n[{self.node_id}] Encerrando atividades do Nó...")
+    def stop(self) -> None:
+        print(f"\n[{self.node_id}] encerrando atividades")
         self.discovery.running = False
         self.monitor.running = False
