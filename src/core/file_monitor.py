93eeda99728867f90e8ba6f6b3e9dac1d6162481
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-06-05 17:41:26 -0300

    refactor: typing code

--- src/core/file_monitor.py
+++ src/core/file_monitor.py
@@ -2,9 +2,16 @@ import os
 import time
 import hashlib
 import threading
+from typing import TypedDict, Optional
+
+class FileMetadata(TypedDict):
+    hash: Optional[str]
+    size: int
+    vector_clock: dict[str, int]
+    deleted: bool
 
 class FileMonitor:
-    def __init__(self, sync_dir, node_id):
+    def __init__(self, sync_dir: str, node_id: str):
         self.sync_dir = sync_dir
         self.node_id = node_id
         self.files = {}
@@ -13,7 +20,7 @@ class FileMonitor:
         if not os.path.exists(self.sync_dir):
             os.makedirs(self.sync_dir)
 
-    def get_file_hash(self, filepath):
+    def get_file_hash(self, filepath: str) -> Optional[str]:
         sha256_hash = hashlib.sha256()
 
         try:
