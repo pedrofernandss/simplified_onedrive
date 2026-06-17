d05e555a1fbc53826933f7e079227fc6bd834294
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-06-17 00:01:01 -0300

    feat: função para indicar conflito entre conteúdo de arquivos de texto

---
+++ src/utils/merge.py
@@ -0,0 +1,50 @@
+import difflib
+import subprocess
+
+def create_merge_conflits_marks(local_content: str, remote_content: str) -> str:
+    local_content_splited = local_content.splitlines()
+    remote_content_splited = remote_content.splitlines()
+
+    marks = []
+    diff = list(difflib.ndiff(local_content_splited, remote_content_splited))
+
+    has_conflits = False
+    local_chunks = []
+    remote_chunks = []
+
+    for line in diff:
+        status = line[:2]
+        text = line[2:]
+
+        if status == '  ':
+            if local_chunks or remote_chunks:
+                marks.append("<<<<<<< SUAS ALTERAÇÕES LOCAIS")
+                marks.extend(local_chunks)
+                marks.append("=======")
+                marks.extend(remote_chunks)
+                marks.append(">>>>>>> ALTERAÇÕES REMOTAS DA REDE")
+                local_chunks, remote_chunks = [], []
+            marks.append(text)
+
+        elif status == '- ':  # Alteração local
+            local_chunks.append(text)
+        elif status == '+ ':  # Alteração remota
+            remote_chunks.append(text)
+
+    if local_chunks or remote_chunks:
+        marks.append("<<<<<<< SUAS ALTERAÇÕES LOCAIS")
+        marks.extend(local_chunks)
+        marks.append("=======")
+        marks.extend(remote_chunks)
+        marks.append(">>>>>>> ALTERAÇÕES REMOTAS DA REDE")
+
+    return "\n".join(marks)
+
+def force_resolution(filepath: str):
+    print(f"\n[AVISO] Conflito detectado no arquivo! Abrindo editor para resolução.")
+    print("Instruções: Remova as linhas com <<<<<<<, =======, >>>>>>> e salve o texto correto.")
+    input("Aperte ENTER para abrir o editor.")
+
+    subprocess.call(["nano", filepath])
+
+    print("[+] Conflito resolvido pelo usuário! Prosseguindo com a sincronização.")
